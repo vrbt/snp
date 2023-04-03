0d84f570ade585a9858f235efd66df3aa51078aa
Tyler Fanelli <tfanelli@redhat.com>
2023-04-03 17:31:24 -0400

    tests: Add milan attestation report verification test
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- tests/certs.rs
+++ tests/certs.rs
@@ -3,6 +3,7 @@
 use snp::certs::{builtin::milan, ca, Certificate, Chain, Verifiable};
 
 const TEST_MILAN_VCEK_DER: &[u8] = include_bytes!("certs_data/vcek_milan.der");
+const TEST_MILAN_ATTESTATION_REPORT: &[u8] = include_bytes!("certs_data/report_milan.hex");
 
 #[cfg(feature = "openssl")]
 #[test]
@@ -16,4 +17,23 @@ fn milan_chain() {
     let chain = Chain { ca, vcek };
 
     chain.verify().unwrap();
+}
+
+#[cfg(feature = "openssl")]
+#[test]
+fn milan_report() {
+    use snp::firmware::guest::types::AttestationReport;
+
+    let ark = milan::ark().unwrap();
+    let ask = milan::ask().unwrap();
+    let vcek = Certificate::from_der(TEST_MILAN_VCEK_DER).unwrap();
+
+    let ca = ca::Chain { ark, ask };
+
+    let chain = Chain { ca, vcek };
+
+    let report_bytes = hex::decode(TEST_MILAN_ATTESTATION_REPORT).unwrap();
+    let report: AttestationReport = unsafe { std::ptr::read(report_bytes.as_ptr() as *const _) };
+
+    (&chain, &report).verify().unwrap();
 }
