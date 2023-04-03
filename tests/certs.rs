b9cb3e1c61769523f85a3897cab3f7c3faa6be6f
Tyler Fanelli <tfanelli@redhat.com>
2023-04-03 15:36:50 -0400

    tests: Add SEV-SNP Milan certificate verification test
    Signed-off-by: Tyler Fanelli <tfanelli@redhat.com>

--- tests/certs.rs
+++ tests/certs.rs
@@ -1,1 +1,19 @@
 // SPDX-License-Identifier: Apache-2.0
+
+use snp::certs::{builtin::milan, ca, Certificate, Chain, Verifiable};
+
+const TEST_MILAN_VCEK_DER: &[u8] = include_bytes!("certs_data/vcek_milan.der");
+
+#[cfg(feature = "openssl")]
+#[test]
+fn milan_chain() {
+    let ark = milan::ark().unwrap();
+    let ask = milan::ask().unwrap();
+    let vcek = Certificate::from_der(TEST_MILAN_VCEK_DER).unwrap();
+
+    let ca = ca::Chain { ark, ask };
+
+    let chain = Chain { ca, vcek };
+
+    chain.verify().unwrap();
+}
